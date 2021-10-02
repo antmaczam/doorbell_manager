d554fb5a6642b4be830a7b4e10eb7d511093de23
Antonio Macías Zambrano <36481170+antmaczam@users.noreply.github.com>
2021-10-02 12:39:42 +0200

    add rpi listener

---
+++ TelegramBot.py
@@ -0,0 +1,55 @@
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from models.AudioPlayer import AudioPlayer, TelegramAudio
+import os
+
+from models.utils import Camera, send_picture, take_picture, DEFAULT_PATH
+
+class TelegramBot():
+  def __init__(self,token):
+      self.token = token
+  
+  def init(self):
+    updater = Updater(self.token)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("comenzar",start))
+    dispatcher.add_handler(CommandHandler("foto",take_picture))
+    dispatcher.add_handler(CommandHandler("video",start_stream))
+    dispatcher.add_handler(CommandHandler("abrir",open_door))
+    dispatcher.add_handler(MessageHandler(Filters.attachment, capture_audio))
+    print('iniciando')
+    updater.start_polling()
+    updater.idle()
+
+def start(update,context):
+  keyboard = [['/foto', '/video'],['/abrir']]
+  msg = '¡Comencemos! A continuación aparecerá la lista de comandos disponibles'
+  reply_markup = ReplyKeyboardMarkup(keyboard,one_time_keyboard=True,resize_keyboard=True)
+  update.message.reply_text(text=msg,reply_markup=reply_markup)
+
+def take_picture(update,context):
+  camera = Camera(DEFAULT_PATH)
+  camera.init_camera()
+  picture_name = camera.take_picture()
+  send_picture(update.message.chat.id,picture_name,os.environ['TOKEN'])
+
+def start_stream(update,context):
+  camera = Camera(DEFAULT_PATH)
+  camera.init_camera()
+  camera.start_stream()
+
+def capture_audio(update,context):
+  if(update.message.voice != None):
+    audio = TelegramAudio(os.environ['TOKEN'],update.message.voice.file_id)
+    audio.create_file()
+    
+    player = AudioPlayer(audio.get_file_name())
+    player.init()
+
+    os.remove(audio.get_file_name())
+  else:
+    print('No telegram audio detected')
+
+def open_door(update,context):
+  print('Sending signal to open de door!')
---
+++ utils.py
@@ -0,0 +1,46 @@
+import telegram
+from models.Camera import Camera
+import os
+import requests
+import RPi.GPIO as GPIO
+import time
+
+DEFAULT_PATH = 'pictures/photo_'
+TOKEN = os.environ['TOKEN']
+CHAT_ID = os.environ['CHAT_ID']
+
+def send_picture(chat_id,path,token):
+  bot = telegram.Bot(token)
+  bot.send_photo(chat_id=chat_id,photo=open(path,"rb"))
+
+def take_picture(path=DEFAULT_PATH):
+  camera = Camera(path)
+  camera.init_camera()
+  return camera.take_picture()
+
+def capture_doorbell():
+  send_message('TIMBRE' + u'🔔')
+  print("Try capturing some signals from RPi!")
+
+def send_message(message):
+  url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&parse_mode=Markdown&text={message}'
+  requests.get(url)
+  
+def listen_to_rpi():
+  try:
+    print("Config rpi pins")
+    pin = 11
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(pin,GPIO.IN)
+    while True:
+      push_pin = GPIO.input(pin)
+      if push_pin == 1:
+        capture_doorbell()
+        send_picture(CHAT_ID,take_picture(),TOKEN)
+        time.sleep(3)
+  except KeyboardInterrupt:
+    GPIO.cleanup()
+    print("\nGPIO clean up")
+    #updater.idle()
+    #print("\nBot stoped")
+
